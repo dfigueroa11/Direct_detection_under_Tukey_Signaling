f2465cfd5395eab675f6fbd98dc43aaae7c2b550
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-05-05 16:37:28 +0200

    print and save equivalence class calculations done and working

--- skripts/equiv_classes_calculator.py
+++ skripts/equiv_classes_calculator.py
@@ -3,31 +3,20 @@ import numpy as np
 
 class Equivalence_classes_calculator:
     constellation = None
+    constellation_name = None
     symbol_block_length = None
     equivalence_classes = None
     
-    def __init__(self,constell,symbol_block_len):
+    def __init__(self,constell,constell_name,symbol_block_len):
         self.constellation =   constell
         self.symbol_block_length = symbol_block_len
+        self.constellation_name = constell_name
 
-    def integrate_and_dump_no_noise(self,symbol_block):
-        ISI_free = np.abs(symbol_block)**2
-        ISI_present = 1/4*np.abs(symbol_block[:-1]+symbol_block[1:])**2+1/8*np.abs(symbol_block[:-1]-symbol_block[1:])**2    
-        return tuple(np.append(ISI_free , ISI_present))
-        
-    def numberToBaseN(self, n):
-        b = len(self.constellation)
-        digits = np.zeros(self.symbol_block_length, dtype=np.int64)
-        i = 0
-        while n:
-            digits[i] = int(n % b)
-            n //= b
-            i = i + 1
-        return digits
-    
-    def generate_symbol_block(self,n):
-        indices = self.numberToBaseN(n)
-        return np.choose(indices,self.constellation)
+    def __init__(self,constell,constell_name):
+        self.constellation =   constell
+        self.constellation_name = constell_name
+
+    ################# main algorithm to find the equivalent classes
 
     def calculate_equivalence_classes(self):
         self.equivalence_classes = {}
@@ -37,20 +26,61 @@ class Equivalence_classes_calculator:
             eqv_class = self.equivalence_classes.get(int_damp_out,np.array([]))
             self.equivalence_classes[int_damp_out] = np.append(eqv_class,symbol_block)
         self.organize_equiv_classes_dictionary()
-    
+
+    def generate_symbol_block(self,n):
+        indices = self.numberToBaseN(n)
+        return np.choose(indices,self.constellation)
+
+    def numberToBaseN(self, n):
+        b = len(self.constellation)
+        digits = np.zeros(self.symbol_block_length, dtype=np.int64)
+        i = 0
+        while n:
+            digits[i] = int(n % b)
+            n //= b
+            i = i + 1
+        return digits
+
+    def integrate_and_dump_no_noise(self,symbol_block):
+        ISI_free = np.abs(symbol_block)**2
+        ISI_present = 1/4*np.abs(symbol_block[:-1]+symbol_block[1:])**2+1/8*np.abs(symbol_block[:-1]-symbol_block[1:])**2    
+        return tuple(np.append(ISI_free , ISI_present))
+
+    ################# formating results from vector to a matrix    
+
     def organize_equiv_classes_dictionary(self):
         # convert the entries of the dictionary from vectors to matrix
         # each row is a symbol_block in the same equivalence class
         for key, value in self.equivalence_classes.items():
             self.equivalence_classes[key] = np.reshape(value,(len(value)//self.symbol_block_length,self.symbol_block_length))
 
-    def summarize_results(self):
+    ################# saving and printing results functions
+    
+    def save_results(self,path):
+        file_name = "EqClasses_" + self.constellation_name + "_n" + str(self.symbol_block_length) + ".npy"
+        np.save(path+file_name,self.equivalence_classes)
+        file_name = "Summary_EqClasses_" + self.constellation_name + "_n" + str(self.symbol_block_length) + ".txt"
+        file = open(path+file_name,"w")
+        self.print_info(file.write)
+        file.close()
+
+    def print_info(self, print_target = print):
+        print_target("For the constellation "+ self.constellation_name + " with points:\n")
+        print_target(np.array2string(self.constellation, precision = 4, separator = "\n",
+                                     prefix="",suffix="\n"))
+        print_target("\nand symbol block length n = "+str(self.symbol_block_length)+"\n")
+        self.summarize_results(print_target)
+
+    def summarize_results(self, print_target = print):
         class_size_cnt = {}
         for key, value in self.equivalence_classes.items():
             class_size = np.shape(value)[0]
             class_size_cnt[class_size] = class_size_cnt.get(class_size,0) + 1
         total_classes = 0
+        print_target("Class size \tNumber of classes\n")
         for key, value in class_size_cnt.items():
-            print(str(key)+" : "+str(value))
+            print_target(str(key)+"\t\t"+str(value)+"\n")
             total_classes += value 
-        print("total amount of classes: " + str(total_classes))
+        rate_loss = 1/self.symbol_block_length*np.log2(len(self.constellation)**self.symbol_block_length/total_classes)
+        print_target("total amount ocf classes equivalence classes: " + str(total_classes)+"\n")
+        print_target("rate loss: "+str(rate_loss)+" [bit/sym]\n")
--- skripts/equivalence_classes_main.py
+++ skripts/equivalence_classes_main.py
@@ -4,15 +4,18 @@ import equiv_classes_calculator
 ################## definition of the communication scheme ##################
 constellation = np.array([1, 1j, -1, -1j])
 constellation = np.append(constellation,constellation*(1+np.sqrt(2)))
-symbol_block_length = 3
+constellation_name = "2-Ring_4-Ary"
+symbol_block_length_start = 3
+symbol_block_length_end = 7
+results_folder = "results/"+constellation_name+"/"
 ############################################################################
 
-eq_classes_calc = equiv_classes_calculator.Equivalence_classes_calculator(constellation,symbol_block_length)
+eq_classes_calc = equiv_classes_calculator.Equivalence_classes_calculator(constellation,constellation_name)
 
-for i in range (3,6):
-    eq_classes_calc.symbol_block_length = i
+for symbol_block_length in range (symbol_block_length_start,symbol_block_length_end+1):
+    eq_classes_calc.symbol_block_length = symbol_block_length
     eq_classes_calc.calculate_equivalence_classes()
-    eq_classes_calc.summarize_results()
+    eq_classes_calc.save_results(results_folder)
 
 
 
