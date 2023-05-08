76a76a7f60567b6cc1708979822b37605776dc36
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-05-08 12:36:37 +0200

    various tests in main

--- skripts/equivalence_classes_main.py
+++ skripts/equivalence_classes_main.py
@@ -1,12 +1,13 @@
 import numpy as np
 import equiv_classes_calculator
+import equiv_class_analyzator
 
 ################## definition of the communication scheme ##################
 constellation = np.array([1, 1j, -1, -1j])
 constellation = np.append(constellation,constellation*(1+np.sqrt(2)))
 constellation_name = "2-Ring_4-Ary"
 symbol_block_length_start = 3
-symbol_block_length_end = 7
+symbol_block_length_end = 3
 results_folder = "results/"+constellation_name+"/"
 ############################################################################
 
@@ -17,7 +18,10 @@ for symbol_block_length in range (symbol_block_length_start,symbol_block_length_
     eq_classes_calc.calculate_equivalence_classes()
     eq_classes_calc.save_results(results_folder)
 
-
-
+eq_class_analyzer = equiv_class_analyzator.Equiv_class_analyzator("results/2-Ring_4-Ary/EqClasses_2-Ring_4-Ary_n3.npy",3)
+eq_cl_size4 = eq_class_analyzer.get_equiv_classes_of_size_n(16)
+for eq_class in range(np.shape(eq_cl_size4)[0]):
+    for sym_blk in range(4):
+        print(eq_classes_calc.integrate_and_dump_no_noise(eq_cl_size4[eq_class,sym_blk,:]))
 
 
