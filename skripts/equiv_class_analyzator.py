285183b4df156cd89833c7283dea57e797f8d814
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-05-08 19:22:50 +0200

    equivalences classes ploting done, and some preliminar tests

--- skripts/equiv_class_analyzator.py
+++ skripts/equiv_class_analyzator.py
@@ -21,11 +21,21 @@ class Equiv_class_analyzator:
                 classes_of_size_n = np.append(classes_of_size_n, [equiv_class], axis=0)
         return classes_of_size_n
     
-    def plot_constellation():
-        pass
+    def plot_constellation(self, constellation, alpha=0.7, c=[[0.7,0.7,0.7]]):
+        plt.scatter(np.real(constellation),np.imag(constellation), alpha=alpha, c=c)
 
-    def plot_symbol_block():
-        pass
+    def plot_symbol_block(self,symbol_block, color='blue', alpha=1):
+        plt.plot(np.real(symbol_block), np.imag(symbol_block), 'o:', color=color, alpha=alpha, fillstyle='none')
+        plt.plot(np.real(symbol_block[0]), np.imag(symbol_block[0]), '*:', color=color)
+        
+    def plot_equivalence_class(self, equivalence_class, alpha=1):
+        for symbol_block in range(np.shape(equivalence_class)[0]):
+            self.plot_symbol_block(equivalence_class[symbol_block,:],color='C{}'.format(symbol_block),alpha=alpha)
 
-    def plot_equivalence_class():
-        pass
+    def plot_all_equivalence_classes(self, equivalence_classes, constellation):
+        for i in range(np.shape(equivalence_classes)[0]):
+            plt.figure(i,(8,8))
+            self.plot_constellation(constellation)
+            self.plot_equivalence_class(equivalence_classes[i,:,:])
+        plt.xlabel("Re")
+        plt.ylabel("Im")
--- skripts/equivalence_classes_main.py
+++ skripts/equivalence_classes_main.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
 import equiv_classes_calculator
 import equiv_class_analyzator
 
@@ -11,17 +14,15 @@ symbol_block_length_end = 3
 results_folder = "results/"+constellation_name+"/"
 ############################################################################
 
-eq_classes_calc = equiv_classes_calculator.Equivalence_classes_calculator(constellation,constellation_name)
+# eq_classes_calc = equiv_classes_calculator.Equivalence_classes_calculator(constellation,constellation_name)
 
-for symbol_block_length in range (symbol_block_length_start,symbol_block_length_end+1):
-    eq_classes_calc.symbol_block_length = symbol_block_length
-    eq_classes_calc.calculate_equivalence_classes()
-    eq_classes_calc.save_results(results_folder)
+# for symbol_block_length in range (symbol_block_length_start,symbol_block_length_end+1):
+#     eq_classes_calc.symbol_block_length = symbol_block_length
+#     eq_classes_calc.calculate_equivalence_classes()
+#     eq_classes_calc.save_results(results_folder)
 
 eq_class_analyzer = equiv_class_analyzator.Equiv_class_analyzator("results/2-Ring_4-Ary/EqClasses_2-Ring_4-Ary_n3.npy",3)
-eq_cl_size4 = eq_class_analyzer.get_equiv_classes_of_size_n(16)
-for eq_class in range(np.shape(eq_cl_size4)[0]):
-    for sym_blk in range(4):
-        print(eq_classes_calc.integrate_and_dump_no_noise(eq_cl_size4[eq_class,sym_blk,:]))
 
 
+eq_class_analyzer.plot_all_equivalence_classes(eq_class_analyzer.get_equiv_classes_of_size_n(8), constellation)
+plt.show()
