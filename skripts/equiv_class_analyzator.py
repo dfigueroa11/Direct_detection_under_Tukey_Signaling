f83b80020c1750ce1c02c1aea0353919ced165c0
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-05-11 16:42:42 +0200

    ploting of classes done, and an interesting analyse implemented

--- skripts/equiv_class_analyzator.py
+++ skripts/equiv_class_analyzator.py
@@ -14,21 +14,37 @@ class Equiv_class_analyzator:
     def load_equiv_classes_file(self, file_name):
         return np.load(file_name, allow_pickle=True).item()
 
-    def get_equiv_classes_of_size_n(self, class_size):
-        classes_of_size_n = np.empty((0,class_size,self.symbol_block_length))
+    def get_equiv_classes_of_size_n(self, class_size, num_classes=0):
+        if not num_classes:
+            print("Give the expected number of classes")
+            return None
+        
+        classes_of_size_n = np.empty((num_classes,class_size,self.symbol_block_length),dtype=np.complex64)
+        i = 0
         for equiv_class in self.equivalence_classes.values():
             if np.shape(equiv_class)[0] == class_size:
-                classes_of_size_n = np.append(classes_of_size_n, [equiv_class], axis=0)
+                classes_of_size_n[i,:,:] = equiv_class
+                i += 1
+            if i == num_classes:
+                return classes_of_size_n
+            
+        print("warning!! fewer classes found than expected")
         return classes_of_size_n
     
-    def plot_constellation(self, constellation, alpha=0.7, c=[[0.7,0.7,0.7]]):
+    def plot_constellation(self, constellation, new_fig=False, figsize=(8,8), alpha=0.7, c=[[0.7,0.7,0.7]]):
+        if new_fig:
+            plt.figure(figsize=figsize)
         plt.scatter(np.real(constellation),np.imag(constellation), alpha=alpha, c=c)
 
-    def plot_symbol_block(self,symbol_block, color='blue', alpha=1):
+    def plot_symbol_block(self,symbol_block, new_fig=False, figsize=(8,8), color='blue', alpha=1):
+        if new_fig:
+            plt.figure(figsize=figsize)
         plt.plot(np.real(symbol_block), np.imag(symbol_block), 'o:', color=color, alpha=alpha, fillstyle='none')
         plt.plot(np.real(symbol_block[0]), np.imag(symbol_block[0]), '*:', color=color)
         
-    def plot_equivalence_class(self, equivalence_class, alpha=1):
+    def plot_equivalence_class(self, equivalence_class, new_fig=False, figsize=(8,8), alpha=1):
+        if new_fig:
+            plt.figure(figsize=figsize)
         for symbol_block in range(np.shape(equivalence_class)[0]):
             self.plot_symbol_block(equivalence_class[symbol_block,:],color='C{}'.format(symbol_block),alpha=alpha)
 
--- skripts/equivalence_classes_ploting.py
+++ skripts/equivalence_classes_ploting.py
@@ -2,17 +2,28 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-import equiv_classes_calculator
 import equiv_class_analyzator
 import constellation_maker as const_mk
 
 
-# eq_class_analyzer = equiv_class_analyzator.Equiv_class_analyzator("results/2-Ring_4-Ary/EqClasses_2-Ring_4-Ary_n3.npy",3)
+def plot_eq_class(file_name, constellation, symbol_block_length, class_size, num_classes, size_classes_selection, size_sym_block_selection):
+    rng = np.random.default_rng()
+    eq_class_analyzer = equiv_class_analyzator.Equiv_class_analyzator(file_name,symbol_block_length)
+    eq_classes_size_n = eq_class_analyzer.get_equiv_classes_of_size_n(class_size,num_classes)
+    select_classes = rng.choice(np.shape(eq_classes_size_n)[0], size=size_classes_selection, replace=False)
+    select_sym_block = rng.choice(class_size, size=size_sym_block_selection, replace=False)
+    eq_classes_size_n_plot = eq_classes_size_n[select_classes,:,:]
+    eq_classes_size_n_plot = eq_classes_size_n_plot[:,select_sym_block,:]
+    eq_class_analyzer.plot_all_equivalence_classes(eq_classes_size_n_plot,constellation)
+    plt.show()
 
 
-# eq_class_analyzer.plot_all_equivalence_classes(eq_class_analyzer.get_equiv_classes_of_size_n(8), constellation)
-# plt.show()
-
-# plt.figure(0, figsize=(8,8))
-# eq_class_analyzer.plot_constellation(const_mk.n_QAM(64))
-# plt.show()
+#########################################################################
+file_name = "results/10-Ring_10-Ary/EqClasses_10-Ring_10-Ary_n3.npy"
+const = const_mk.n_ring_m_ary_phase([1,2,3,4,5,6,7,8,9,10],10)
+sym_block_len = 3
+class_size = 40
+size_sym_block_selection = 10
+num_classes = 20250
+size_classes_selection = 10
+plot_eq_class(file_name, const, sym_block_len, class_size, num_classes,size_classes_selection,size_sym_block_selection)
