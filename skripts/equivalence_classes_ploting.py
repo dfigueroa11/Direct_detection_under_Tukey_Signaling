889c27beb670834e973dd240423d762dcd862853
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-05-11 11:03:07 +0200

    make a script to analyze equvalence clases and ploting things

---
+++ skripts/equivalence_classes_ploting.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+import equiv_classes_calculator
+import equiv_class_analyzator
+import constellation_maker as const_mk
+
+
+# eq_class_analyzer = equiv_class_analyzator.Equiv_class_analyzator("results/2-Ring_4-Ary/EqClasses_2-Ring_4-Ary_n3.npy",3)
+
+
+# eq_class_analyzer.plot_all_equivalence_classes(eq_class_analyzer.get_equiv_classes_of_size_n(8), constellation)
+# plt.show()
+
+# plt.figure(0, figsize=(8,8))
+# eq_class_analyzer.plot_constellation(const_mk.n_QAM(64))
+# plt.show()
