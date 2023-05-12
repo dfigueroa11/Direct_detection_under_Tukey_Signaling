fc730e38e549a04d8a738e059596835223f7db58
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-05-12 17:18:19 +0200

    modify get_equiv_classes_of_size_n, no more needed the number of classes

--- skripts/equiv_class_analyzator.py
+++ skripts/equiv_class_analyzator.py
@@ -6,30 +6,24 @@ class Equiv_class_analyzator:
     
     equivalence_classes = None
     symbol_block_length = None
+    num_equiv_classes = None
 
     def __init__(self, file_name,symbol_block_length):
         self.equivalence_classes = self.load_equiv_classes_file(file_name)
         self.symbol_block_length = symbol_block_length
+        self.num_equiv_classes = len(self.equivalence_classes)
 
     def load_equiv_classes_file(self, file_name):
         return np.load(file_name, allow_pickle=True).item()
 
-    def get_equiv_classes_of_size_n(self, class_size, num_classes=0):
-        if not num_classes:
-            print("Give the expected number of classes")
-            return None
-        
-        classes_of_size_n = np.empty((num_classes,class_size,self.symbol_block_length),dtype=np.complex64)
+    def get_equiv_classes_of_size_n(self, class_size):
+        classes_of_size_n = np.empty((self.num_equiv_classes, class_size, self.symbol_block_length), dtype=np.complex64)
         i = 0
         for equiv_class in self.equivalence_classes.values():
             if np.shape(equiv_class)[0] == class_size:
                 classes_of_size_n[i,:,:] = equiv_class
                 i += 1
-            if i == num_classes:
-                return classes_of_size_n
-            
-        print("warning!! fewer classes found than expected")
-        return classes_of_size_n
+        return classes_of_size_n[0:i,:,:]
     
     def plot_constellation(self, constellation, new_fig=False, figsize=(8,8), alpha=0.7, c=[[0.7,0.7,0.7]]):
         if new_fig:
@@ -41,6 +35,8 @@ class Equiv_class_analyzator:
             plt.figure(figsize=figsize)
         plt.plot(np.real(symbol_block), np.imag(symbol_block), 'o:', color=color, alpha=alpha, fillstyle='none')
         plt.plot(np.real(symbol_block[0]), np.imag(symbol_block[0]), '*:', color=color)
+        plt.plot(np.real(symbol_block[-1]), np.imag(symbol_block[-1]), 'o:', color=color)
+        
         
     def plot_equivalence_class(self, equivalence_class, new_fig=False, figsize=(8,8), alpha=1):
         if new_fig:
