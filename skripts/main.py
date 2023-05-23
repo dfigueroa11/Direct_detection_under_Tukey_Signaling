427a99ba66cc60285a54357a271fe68055ad7247
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-05-23 12:38:36 +0200

    organization of folders

---
+++ skripts/main.py
@@ -0,0 +1,29 @@
+import numpy as np
+import scipy.signal as sig
+import matplotlib.pyplot as plt 
+import time
+
+import sys 
+sys.path.insert(1, 'communications_system')
+sys.path.insert(2, 'Equivalence_classes')
+import signaling
+import constellation_maker
+
+sig_block_1 = signaling.Signaling_block(symbol_time=1, sps=21, beta=0.5)
+# plt.figure(0)
+# plt.stem(sig_block_1.time_vec,sig_block_1.tukey_window)
+# plt.grid(visible=True)
+
+constellation = constellation_maker.n_ring_m_ary_phase([1,2,3,4],4)
+symbols = constellation[np.random.choice(len(constellation), size=int(30))]
+
+signal = sig_block_1.generate_signal(symbols=symbols)
+
+plt.figure(0)
+plt.stem(np.real(symbols))
+plt.stem(np.imag(symbols))
+
+plt.figure(1)
+plt.plot(np.real(signal))
+plt.plot(np.imag(signal))
+plt.show()
