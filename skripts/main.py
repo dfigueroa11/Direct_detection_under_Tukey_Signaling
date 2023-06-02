8e957a7bbf8c85bec66d53d0360188e2fc8e6e4a
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-06-02 16:49:50 +0200

    done up to integrate & dump

--- skripts/main.py
+++ skripts/main.py
@@ -1,7 +1,9 @@
+import time
 import numpy as np
 import scipy.signal as sig
 import matplotlib.pyplot as plt 
 
+import constellation_maker as const_mk
 import class_representative
 import signaling
 import photodiode
@@ -9,20 +11,24 @@ import integrate_dump
 
 
 file_name = "communications_system/representative_classes/2-Ring_4-Ary_n3.npy"
-class_rep_block = class_representative.Class_representative_block(file_name)
-sig_block = signaling.Signaling_block(symbol_time=1, sps=10, beta=0.6, adjust_beta=True)
+constellation = const_mk.n_ring_m_ary_phase([1,2],4)
+sym_block_len = 3
+class_rep_block = class_representative.Class_representative_block(file_name,constellation,sym_block_len)
+sig_block = signaling.Signaling_block(symbol_time=1, sps=11, beta=0.6, adjust_beta=True)
 photodiode_block = photodiode.Photodiode(responsivity=1, sigma2_sh=0, sigma_2_th=0)
 int_dump_block = integrate_dump.Integrate_dump_block(sig_block)
 
-# rng = np.random.default_rng()
-# N_sym_blocks = 10
+rng = np.random.default_rng(4)
+N_sym_blocks = 100
 
-# k_vec = rng.choice(class_rep_block.representative_class_size, N_sym_blocks)
-# symbols = class_rep_block.get_symbol_blocks(k_vec)
-# tx_signal = sig_block.generate_signal(symbols)
-# rx_signal = photodiode_block.square_law_detection(tx_signal)
-# y,z = int_dump_block.integrate_dump(rx_signal)
-
+s = time.time()
+k_vec = rng.choice(class_rep_block.representative_class_size, N_sym_blocks)
+symbols = class_rep_block.get_symbol_blocks(k_vec)
+tx_signal = sig_block.generate_signal(symbols)
+rx_signal = photodiode_block.square_law_detection(tx_signal)
+y,z = int_dump_block.integrate_dump(rx_signal)
+e = time.time()
+print(e-s)
 
 # plt.figure(1)
 # plt.stem(sig_block.time_vec,sig_block.tukey_window)
@@ -30,12 +36,6 @@ int_dump_block = integrate_dump.Integrate_dump_block(sig_block)
 
 # plt.figure(2)
 # plt.stem(np.arange(len(rx_signal))*sig_block.Ts, rx_signal)
-
-# plt.figure(3)
-# plt.stem(y)
-
-# plt.figure(4)
-# plt.stem(z)
 
 
 # plt.show()
