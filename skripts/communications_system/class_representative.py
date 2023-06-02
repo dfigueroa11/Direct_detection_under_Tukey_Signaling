c32ca3e1e172b04e308491663c0b7bacb9522552
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-06-02 15:34:48 +0200

    system up to integrate & dump working properly

--- skripts/communications_system/class_representative.py
+++ skripts/communications_system/class_representative.py
@@ -3,16 +3,32 @@ import numpy as np
 class Class_representative_block:
 
     representative_class = None
-    symbol_block_lenght = None
+    symbol_block_length = None
     representative_class_size = None
+    constellation = None
 
-    def __init__(self, rep_class_file):
+    def __init__(self, rep_class_file, constellation, symbol_block_len):
         self.representative_class = np.load(rep_class_file)
-        self.symbol_block_lenght = np.shape(self.representative_class)[1]
-        self.representative_class_size = np.shape(self.representative_class)[0]
+        self.symbol_block_length = symbol_block_len
+        self.constellation = constellation
+        self.representative_class_size = len(self.representative_class)
     
     def get_symbol_blocks(self, k_vec):
-        symbol_blocks = np.empty(shape=self.symbol_block_lenght*len(k_vec),dtype=complex)
+        symbol_blocks = np.empty(shape=self.symbol_block_length*len(k_vec),dtype=complex)
         for i,k in enumerate(k_vec):
-            symbol_blocks[i*self.symbol_block_lenght:(i+1)*self.symbol_block_lenght] = self.representative_class[k,:]
+            symbol_blocks[i*self.symbol_block_length:(i+1)*self.symbol_block_length] = self.generate_symbol_block(self.representative_class[k])
         return symbol_blocks
+    
+    def generate_symbol_block(self,num):
+        indices = self.numberToBaseN(num)
+        return self.constellation[indices]
+
+    def numberToBaseN(self, num):
+        b = len(self.constellation)
+        digits = np.zeros(self.symbol_block_length, dtype=np.int32)
+        i = 0
+        while num:
+            digits[i] = int(num % b)
+            num //= b
+            i = i + 1
+        return digits
--- skripts/main.py
+++ skripts/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.signal as sig
 import matplotlib.pyplot as plt 
 
+import constellation_maker as const_mk
 import class_representative
 import signaling
 import photodiode
@@ -9,33 +10,29 @@ import integrate_dump
 
 
 file_name = "communications_system/representative_classes/2-Ring_4-Ary_n3.npy"
-class_rep_block = class_representative.Class_representative_block(file_name)
-sig_block = signaling.Signaling_block(symbol_time=1, sps=10, beta=0.6, adjust_beta=True)
+constellation = const_mk.n_ring_m_ary_phase([1,2],4)
+sym_block_len = 3
+class_rep_block = class_representative.Class_representative_block(file_name,constellation,sym_block_len)
+sig_block = signaling.Signaling_block(symbol_time=1, sps=11, beta=0.6, adjust_beta=True)
 photodiode_block = photodiode.Photodiode(responsivity=1, sigma2_sh=0, sigma_2_th=0)
 int_dump_block = integrate_dump.Integrate_dump_block(sig_block)
 
-# rng = np.random.default_rng()
-# N_sym_blocks = 10
+rng = np.random.default_rng()
+N_sym_blocks = 10
 
-# k_vec = rng.choice(class_rep_block.representative_class_size, N_sym_blocks)
-# symbols = class_rep_block.get_symbol_blocks(k_vec)
-# tx_signal = sig_block.generate_signal(symbols)
-# rx_signal = photodiode_block.square_law_detection(tx_signal)
-# y,z = int_dump_block.integrate_dump(rx_signal)
-
-
-# plt.figure(1)
-# plt.stem(sig_block.time_vec,sig_block.tukey_window)
+k_vec = rng.choice(class_rep_block.representative_class_size, N_sym_blocks)
+symbols = class_rep_block.get_symbol_blocks(k_vec)
+tx_signal = sig_block.generate_signal(symbols)
+rx_signal = photodiode_block.square_law_detection(tx_signal)
+y,z = int_dump_block.integrate_dump(rx_signal)
 
 
-# plt.figure(2)
-# plt.stem(np.arange(len(rx_signal))*sig_block.Ts, rx_signal)
-
-# plt.figure(3)
-# plt.stem(y)
-
-# plt.figure(4)
-# plt.stem(z)
+plt.figure(1)
+plt.stem(sig_block.time_vec,sig_block.tukey_window)
 
 
-# plt.show()
+plt.figure(2)
+plt.stem(np.arange(len(rx_signal))*sig_block.Ts, rx_signal)
+
+
+plt.show()
