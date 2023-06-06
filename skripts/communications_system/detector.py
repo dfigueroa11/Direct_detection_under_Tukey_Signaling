5c1fdddce979215fabe3b0b733e77485f87d7372
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-06-06 13:11:31 +0200

    speed test: 4.179329872131348 s

--- skripts/communications_system/detector.py
+++ skripts/communications_system/detector.py
@@ -10,6 +10,11 @@ class Detector_block:
     responsivity = None
     sigma2_sh = None
     sigma2_th = None
+    __means_y__ = None
+    __vars_y__ = None
+    __means_z__ = None
+    __vars_z__ = None
+
 
     def __init__(self, symbol_time, beta, symbol_block_len, representative_class, responsivity, sigma2_sh, sigma2_th):
         self.symbol_time = symbol_time
@@ -20,28 +25,29 @@ class Detector_block:
         self.responsivity = responsivity
         self.sigma2_sh = sigma2_sh
         self.sigma2_th = sigma2_th
+        
+        norm_x = np.square(np.abs(self.representative_class))
+        self.__means_y__ = self.alpha2*(1-self.beta)*self.symbol_time*norm_x
+        self.__vars_y__ = (1-self.beta)*self.symbol_time*(self.alpha2*norm_x*self.sigma2_sh+self.sigma2_th)
+        phi = (1/4*np.square(np.abs(self.representative_class[:,:-1]+self.representative_class[:,1:]))
+                    +1/8*np.square(np.abs(self.representative_class[:,:-1]-self.representative_class[:,1:])))
+        self.__means_z__ = self.alpha2*self.beta*self.symbol_time*phi
+        self.__vars_z__ = self.beta*self.symbol_time*(self.alpha2*phi*self.sigma2_sh+self.sigma2_th)
 
     def decode(self,y,z,num_sym_blocks):
-        k_hat = -np.ones(num_sym_blocks, dtype=int)
-        for rx_sym_block in range(num_sym_blocks):
-            liklyhood = self.liklyhood_y_z_given_xd(y[self.symbol_block_len*rx_sym_block:self.symbol_block_len*(rx_sym_block+1)],
-                                                    z[self.symbol_block_len*rx_sym_block:self.symbol_block_len*(rx_sym_block+1)-1])
-            k_hat[rx_sym_block] = np.argmax(liklyhood)
-        return k_hat
+        y = np.reshape(y,(num_sym_blocks,self.symbol_block_len))
+        z = np.reshape(z,(num_sym_blocks,self.symbol_block_len))
+        z = z[:,:-1]
+        return np.argmax(self.liklyhood_y_z_given_xd(y,z), axis=1)
 
-    def liklyhood_y_z_given_xd(self,y,z):
-        return np.prod(self.liklyhood_yk_given_xk(y), axis=1)*np.prod(self.liklyhood_zl_given_xl_xl1(z), axis=1)
+    def liklyhood_y_z_given_xd(self,ys,zs):
+        return [np.prod(self.liklyhood_yk_given_xk(y), axis=1)*np.prod(self.liklyhood_zl_given_xl_xl1(z), axis=1) for y,z in zip(ys,zs)]
     
-    def liklyhood_yk_given_xk(self,y):
-        norm_x = np.square(np.abs(self.representative_class))
-        return self.norm_dist(y, means=self.alpha2*(1-self.beta)*self.symbol_time*norm_x,
-                              vars=(1-self.beta)*self.symbol_time*(self.alpha2*norm_x*self.sigma2_sh+self.sigma2_th))
+    def liklyhood_yk_given_xk(self,y): 
+        return self.norm_dist(y, means=self.__means_y__, vars=self.__vars_y__)
     
     def liklyhood_zl_given_xl_xl1(self,z):
-        phi = (1/4*np.square(np.abs(self.representative_class[:,:-1]+self.representative_class[:,1:]))
-               +1/8*np.square(np.abs(self.representative_class[:,:-1]-self.representative_class[:,1:])))
-        return self.norm_dist(z, means=self.alpha2*self.beta*self.symbol_time*phi,
-                                vars=self.beta*self.symbol_time*(self.alpha2*phi*self.sigma2_sh+self.sigma2_th))
+        return self.norm_dist(z, means=self.__means_z__, vars=self.__vars_z__)
     
     def norm_dist(self, x, means, vars):
         return [1/np.sqrt(2*np.pi*var)*np.exp(-(x-mean)**2/(2*var)) for mean,var in zip(means,vars)]
