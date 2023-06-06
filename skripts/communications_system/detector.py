bf86dcf25893faf037c0534d2375d1afc2c30793
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-06-06 15:35:39 +0200

    using logliklyhood and simplifications

--- skripts/communications_system/detector.py
+++ skripts/communications_system/detector.py
@@ -38,10 +38,10 @@ class Detector_block:
         y = np.reshape(y,(num_sym_blocks,self.symbol_block_len))
         z = np.reshape(z,(num_sym_blocks,self.symbol_block_len))
         z = z[:,:-1]
-        return np.argmax(self.liklyhood_y_z_given_xd(y,z), axis=1)
+        return np.argmin(self.liklyhood_y_z_given_xd(y,z), axis=1)
 
     def liklyhood_y_z_given_xd(self,ys,zs):
-        return [np.prod(self.liklyhood_yk_given_xk(y), axis=1)*np.prod(self.liklyhood_zl_given_xl_xl1(z), axis=1) for y,z in zip(ys,zs)]
+        return [np.sum(self.liklyhood_yk_given_xk(y), axis=1)+np.sum(self.liklyhood_zl_given_xl_xl1(z), axis=1) for y,z in zip(ys,zs)]
     
     def liklyhood_yk_given_xk(self,y): 
         return self.norm_dist(y, means=self.__means_y__, vars=self.__vars_y__)
@@ -50,4 +50,4 @@ class Detector_block:
         return self.norm_dist(z, means=self.__means_z__, vars=self.__vars_z__)
     
     def norm_dist(self, x, means, vars):
-        return [1/np.sqrt(2*np.pi*var)*np.exp(-(x-mean)**2/(2*var)) for mean,var in zip(means,vars)]
+        return [np.log(var) + (x-mean)**2/var for mean,var in zip(means,vars)]
