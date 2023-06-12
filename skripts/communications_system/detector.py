3b9f70fd19567ce3142ed1b13cec2eeae4b417d3
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-06-12 11:35:23 +0200

    fix variable name

--- skripts/communications_system/detector.py
+++ skripts/communications_system/detector.py
@@ -27,9 +27,9 @@ class Detector_block:
         
     def set_representative_class(self,representative_class):
         self.representative_class = representative_class
-        norm_x = np.square(np.abs(self.representative_class))
-        self.__means_y__ = self.alpha2*(1-self.beta)*self.symbol_time*norm_x
-        self.__vars_y__ = (1-self.beta)*self.symbol_time*(self.alpha2*norm_x*self.sigma2_sh+self.sigma2_th)
+        power_x = np.square(np.abs(self.representative_class))
+        self.__means_y__ = self.alpha2*(1-self.beta)*self.symbol_time*power_x
+        self.__vars_y__ = (1-self.beta)*self.symbol_time*(self.alpha2*power_x*self.sigma2_sh+self.sigma2_th)
         phi = (1/4*np.square(np.abs(self.representative_class[:,:-1]+self.representative_class[:,1:]))
                +1/8*np.square(np.abs(self.representative_class[:,:-1]-self.representative_class[:,1:])))
         self.__means_z__ = self.alpha2*self.beta*self.symbol_time*phi
