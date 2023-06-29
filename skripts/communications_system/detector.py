c4fd8521c59868d793f10bb904325291ce794891
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-06-29 16:29:41 +0200

    small correction to variance, the performance is better

--- skripts/communications_system/detector.py
+++ skripts/communications_system/detector.py
@@ -29,18 +29,19 @@ class Detector_block:
         self.representative_class = representative_class
         power_x = np.square(np.abs(self.representative_class))
         self.__means_y__ = self.responsivity*self.alpha2*(1-self.beta)*self.symbol_time*power_x
-        self.__vars_y__ = (1-self.beta)*self.symbol_time*(self.alpha2*power_x*self.sigma2_sh+self.sigma2_th)
+        self.__vars_y__ = ((1-self.beta)*self.symbol_time)**2*(self.alpha2*power_x*self.sigma2_sh+self.sigma2_th)
         phi = (1/4*np.square(np.abs(self.representative_class[:,:-1]+self.representative_class[:,1:]))
                +1/8*np.square(np.abs(self.representative_class[:,:-1]-self.representative_class[:,1:])))
         self.__means_z__ = self.responsivity*self.alpha2*self.beta*self.symbol_time*phi
-        self.__vars_z__ = self.beta*self.symbol_time*(self.alpha2*phi*self.sigma2_sh+self.sigma2_th)
+        self.__vars_z__ = (self.beta*self.symbol_time)**2*(self.alpha2*phi*self.sigma2_sh+self.sigma2_th)
 
 
-    def decode(self,y,z,num_sym_blocks):
+    def decode_logliklyhood(self,y,z,num_sym_blocks):
         y = np.reshape(y,(num_sym_blocks,self.symbol_block_len))
         z = np.reshape(z,(num_sym_blocks,self.symbol_block_len))
         z = z[:,:-1]
-        return np.argmin(self.logliklyhood_y_z_given_xd(y,z), axis=1)
+        xd = self.logliklyhood_y_z_given_xd(y,z)
+        return np.argmin(xd, axis=1)
 
     def logliklyhood_y_z_given_xd(self,ys,zs):
         return [np.sum(self.logliklyhood_yk_given_xk(y), axis=1)+np.sum(self.logliklyhood_zl_given_xl_xl1(z), axis=1) for y,z in zip(ys,zs)]
@@ -53,3 +54,23 @@ class Detector_block:
     
     def log_norm_dist(self, x, means, vars):
         return [np.log(var) + (x-mean)**2/var for mean,var in zip(means,vars)]
+    
+    def decode_liklyhood(self,y,z,num_sym_blocks):
+        y = np.reshape(y,(num_sym_blocks,self.symbol_block_len))
+        z = np.reshape(z,(num_sym_blocks,self.symbol_block_len))
+        z = z[:,:-1]
+        xd = self.liklyhood_y_z_given_xd(y,z)
+        #print(xd)
+        return np.argmax(xd, axis=1)
+
+    def liklyhood_y_z_given_xd(self,ys,zs):
+        return [np.prod(self.liklyhood_yk_given_xk(y), axis=1)*np.prod(self.liklyhood_zl_given_xl_xl1(z), axis=1) for y,z in zip(ys,zs)]
+    
+    def liklyhood_yk_given_xk(self,y): 
+        return self.norm_dist(y, means=self.__means_y__, vars=self.__vars_y__)
+    
+    def liklyhood_zl_given_xl_xl1(self,z):
+        return self.norm_dist(z, means=self.__means_z__, vars=self.__vars_z__)
+    
+    def norm_dist(self, x, means, vars):
+        return [1/np.sqrt(2*np.pi*var)*np.exp(-(x-mean)**2/(2*var)) for mean,var in zip(means,vars)]
--- skripts/main.py
+++ skripts/main.py
@@ -12,13 +12,13 @@ import detector
 ########################## Problem definition #####################################
 file_name = "communications_system/representative_classes/2-4SQAM_n3.npy"
 sym_block_len = 3
-sym_time = 1
+sym_time = 0.0000001
 sps = 21
 beta = 0.5
 responsivity = 10
 sigma2_sh = photodiode.get_sigma2_sh(M_APD=20, F=12.78, R_APD=10*5e9)#3e-6#photodiode.get_sigma2_sh(M_APD=20, F=12.78, R_APD=1) # values from the paper
 sigma2_th = photodiode.get_sigma2_th(Tk=300*5e9, RL=15)#3e-12#photodiode.get_sigma2_th(Tk=300, RL=15) # values from the paper
-ideal = False
+ideal = True
 print(f"shot noise power\t{sigma2_sh: .3e}")
 print(f"thermal noise power\t{sigma2_th: .3e}")
 
@@ -41,12 +41,12 @@ class_rep_block.set_up_const_and_rep_class(constellation)
 detector_block.set_representative_class(class_rep_block.representative_class)
 s = time.time()
 rng = np.random.default_rng(rng_seed)
-k_tx = rng.choice(len(class_rep_block.representative_class), N_sym_blocks)
+k_tx = [4]*N_sym_blocks#rng.choice(len(class_rep_block.representative_class), N_sym_blocks)
 symbols = class_rep_block.get_symbol_blocks(k_tx)
 tx_signal = sig_block.generate_signal(symbols)
 rx_signal = photodiode_block.square_law_detection(tx_signal)
 y,z = int_dump_block.integrate_dump(rx_signal)
-k_rx = detector_block.decode(y,z,N_sym_blocks)
+k_rx = detector_block.decode_liklyhood(y,z,N_sym_blocks)
 e = time.time()
 sig_power = np.trapz(np.abs(tx_signal)**2, dx=sig_block.Ts)/(sig_block.Ts*len(tx_signal))
 sig_power = 10*np.log10(sig_power)+30
@@ -54,18 +54,26 @@ noise_power = np.trapz(np.abs(np.abs(tx_signal)**2*responsivity-rx_signal)**2, d
 print(f"signal power: {sig_power: .2f} dBm")
 print(f"noise power: {noise_power: .3e}")
 print(f"simulation time: {e-s: .3f}")
-print(f"SER: {np.mean(k_tx != k_rx)}")
+print(f"SER: {np.mean(k_tx != k_rx): .3e}")
 
 y = np.reshape(y,(N_sym_blocks,3))
 z = np.reshape(z,(N_sym_blocks,3))
 z = z[:,:-1]
 
 
-# print(np.allclose(np.mean(y, axis=0),detector_block.__means_y__[k_tx[0],:], rtol=1e-2))
-# print(np.allclose(np.mean(z, axis=0),detector_block.__means_z__[k_tx[0],:], rtol=1e-2))
+print(np.allclose(np.mean(y, axis=0),detector_block.__means_y__[k_tx[0],:], rtol=1e-2))
+print(np.allclose(np.mean(z, axis=0),detector_block.__means_z__[k_tx[0],:], rtol=1e-2))
+# print(np.mean(y, axis=0))
+# print(detector_block.__means_y__[k_tx[0],:])
+# print(np.mean(z, axis=0))
+# print(detector_block.__means_z__[k_tx[0],:])
 
-# print(np.allclose(np.var(y, axis=0),detector_block.__vars_y__[k_tx[0],:]))
-# print(np.allclose(np.var(z, axis=0),detector_block.__vars_z__[k_tx[0],:]))
+print(np.allclose(np.var(y, axis=0),detector_block.__vars_y__[k_tx[0],:], rtol=1e-2, atol=0))
+print(np.allclose(np.var(z, axis=0),detector_block.__vars_z__[k_tx[0],:], rtol=1e-2, atol=0))
+print(np.var(y, axis=0))
+print(detector_block.__vars_y__[k_tx[0],:])
+print(np.var(z, axis=0))
+print(detector_block.__vars_z__[k_tx[0],:])
 
 
 # plt.figure(1)
@@ -78,4 +86,4 @@ z = z[:,:-1]
 # plt.stem(z)
 
 
-plt.show()
+# plt.show()
