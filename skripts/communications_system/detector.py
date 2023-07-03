3549ce50f8c554ad3b2473f2770b013e79123f1b
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-07-03 10:30:16 +0200

    include the factor Ts to de variances due to the discrete simulation

--- skripts/communications_system/detector.py
+++ skripts/communications_system/detector.py
@@ -3,6 +3,7 @@ import numpy as np
 class Detector_block:
 
     symbol_time = None
+    Ts = None
     beta = None 
     alpha = None
     symbol_block_len = None
@@ -16,8 +17,9 @@ class Detector_block:
     __vars_z__ = None
 
 
-    def __init__(self, symbol_time, beta, symbol_block_len, responsivity, sigma2_sh, sigma2_th):
+    def __init__(self, symbol_time, Ts, beta, symbol_block_len, responsivity, sigma2_sh, sigma2_th):
         self.symbol_time = symbol_time
+        self.Ts = Ts
         self.beta = beta
         self.alpha2 = 4/(4-self.beta)
         self.symbol_block_len = symbol_block_len
@@ -29,11 +31,11 @@ class Detector_block:
         self.representative_class = representative_class
         power_x = np.square(np.abs(self.representative_class))
         self.__means_y__ = self.responsivity*self.alpha2*(1-self.beta)*self.symbol_time*power_x
-        self.__vars_y__ = ((1-self.beta)*self.symbol_time)*(self.alpha2*power_x*self.sigma2_sh+self.sigma2_th)
+        self.__vars_y__ = ((1-self.beta)*self.symbol_time)*self.Ts*(self.alpha2*power_x*self.sigma2_sh+self.sigma2_th)
         phi = (1/4*np.square(np.abs(self.representative_class[:,:-1]+self.representative_class[:,1:]))
                +1/8*np.square(np.abs(self.representative_class[:,:-1]-self.representative_class[:,1:])))
         self.__means_z__ = self.responsivity*self.alpha2*self.beta*self.symbol_time*phi
-        self.__vars_z__ = (self.beta*self.symbol_time)*(self.alpha2*phi*self.sigma2_sh+self.sigma2_th)
+        self.__vars_z__ = (self.beta*self.symbol_time)*self.Ts*(self.alpha2*phi*self.sigma2_sh+self.sigma2_th)
 
 
     def decode_logliklyhood(self,y,z,num_sym_blocks):
