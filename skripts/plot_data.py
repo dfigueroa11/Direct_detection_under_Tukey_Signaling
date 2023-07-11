8e84292c305f08737179ff6a41bf12e817c25637
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-07-11 15:14:45 +0200

    ploting some data

--- skripts/plot_data.py
+++ skripts/plot_data.py
@@ -1,66 +1,33 @@
 import numpy as np
+import pickle
 import matplotlib.pyplot as plt
 
-files_ser = ("2-4SQAM_n4_M256_b 30_SER.npy",
-             "2-4SQAM_n4_M256_b 50_SER.npy",
-             "2-4SQAM_n4_M256_b 60_SER.npy",
-             "2-4SQAM_n4_M256_b 70_SER.npy",
-             "2-4SQAM_n4_M256_b 90_SER.npy")
-
-files_ber = ("2-4SQAM_n4_M256_b 30_BER.npy",
-             "2-4SQAM_n4_M256_b 50_BER.npy",
-             "2-4SQAM_n4_M256_b 60_BER.npy",
-             "2-4SQAM_n4_M256_b 70_BER.npy",
-             "2-4SQAM_n4_M256_b 90_BER.npy")
-
-files_mi = ("2-4SQAM_n3_M72_b 10_MI.npy",
-            "2-4SQAM_n3_M72_b 50_MI.npy",
-            "2-4SQAM_n3_M72_b 70_MI.npy",
-            "2-4SQAM_n3_M72_b 90_MI.npy")
-
-
-betas = np.array([0.3,0.5,0.6,0.7,0.9])
-optical_power_range = np.arange(-17,-6)
-
-betas_mi = np.array([0.1,0.5,0.7,0.9])
-optical_power_range_mi = np.arange(-33,-4)
-
-
-ser_results = np.empty((len(betas),len(optical_power_range)))
-ber_results = np.empty((len(betas),len(optical_power_range)))
-mi_results = np.empty((len(betas_mi),len(optical_power_range_mi)))
-
-
-for i,file in enumerate(files_ber):
-    ber_results[i,:] = np.load("whole_system_sim_results/"+file)
-for i,file in enumerate(files_ser):
-    ser_results[i,:] = np.load("whole_system_sim_results/"+file)
-for i,file in enumerate(files_mi):
-    mi_results[i,:] = np.load("whole_system_sim_results/"+file)
+sys_2_4_SQAM_n3 = pickle.load(open("whole_system_sim_results/2-4SQAM_n3_all_MI.p","rb"))
+sys_2_4_SQAM_n4 = pickle.load(open("whole_system_sim_results/2-4SQAM_n4_256.p","rb"))
 
 
 plt.figure(0)
-for ber,beta in zip(ber_results,betas):
-    plt.semilogy(optical_power_range, ber, label=f"beta={beta}")
+for ber,beta in zip(sys_2_4_SQAM_n4["ber"],sys_2_4_SQAM_n4["betas"]):
+    plt.semilogy(sys_2_4_SQAM_n4["rop_range_dBm"], ber, label=f"beta={beta}")
 plt.legend()
 plt.grid()
 plt.ylabel("BER")
 plt.xlabel("ROP [dBm]")
 plt.ylim(bottom=1e-4)
 plt.xlim(-17,-7)
-plt.xticks(optical_power_range)
+plt.xticks(sys_2_4_SQAM_n4["rop_range_dBm"])
 
 
 plt.figure(1)
-for mi,beta in zip(mi_results,betas_mi):
-    plt.plot(optical_power_range_mi, mi, label=f"beta={beta}")
+for mi,beta in zip(sys_2_4_SQAM_n3["mi"],sys_2_4_SQAM_n3["betas"]):
+    plt.plot(sys_2_4_SQAM_n3["rop_range_dBm"], mi, label=f"beta={beta}")
 plt.legend()
 plt.grid()
 plt.ylabel("MI")
 plt.xlabel("ROP [dBm]")
 plt.ylim(0,(3-0.9433583328525624)*1.05)
 plt.xlim(-33,-5)
-plt.xticks(optical_power_range_mi[::2])
+plt.xticks(sys_2_4_SQAM_n3["rop_range_dBm"][::2])
 
 
 plt.show()
