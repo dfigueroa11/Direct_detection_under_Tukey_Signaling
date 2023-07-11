11d7ab647bae7127ac4795c303a200789578d677
Diego  Figueroa del castillo <dfigueroa@unal.edu.co>
2023-07-11 13:23:04 +0200

    Organization of the code to facilitate simulations and readability

---
+++ skripts/communications_system/tukey_signaling.py
@@ -0,0 +1,88 @@
+import numpy as np
+import pickle
+
+import constellation_maker as const_mk
+import class_representative
+import signaling
+import photodiode
+import integrate_dump
+import detector
+from channel_metrics import get_BER, get_MI, get_SER
+
+
+class Tukey_signaling: 
+
+    file_name = None
+    sym_block_len = None
+    baud_rate = None
+    sym_time = None
+    sps = None
+    fs = None
+    responsivity = None
+    sigma2_sh = None
+    sigma2_th = None
+    ideal = None
+    beta = None
+    constellation = None
+
+    class_rep_block = None
+    sig_block = None
+    photodiode_block = None
+    int_dump_block = None
+    detector_block = None
+
+
+    def __init__(self, file_name, sym_block_len, baud_rate, sps, responsivity, N0_sh, N0_th, constellation, ideal=False):
+        self.file_name = file_name
+        self.sym_block_len = sym_block_len
+        self.baud_rate = baud_rate
+        self.sym_time = 1/self.baud_rate
+        self.sps = sps
+        self.fs = (self.sps-1)/self.sym_time
+        self.responsivity = responsivity
+        self.sigma2_sh = N0_sh*self.fs
+        self.sigma2_th = N0_th*self.fs
+        self.constellation = constellation
+        self.ideal = ideal
+
+    def setup_system(self, beta, rng_seed, adjust_beta=True):
+        self.class_rep_block = class_representative.Class_representative_block(self.file_name,self.sym_block_len)
+        self.sig_block = signaling.Signaling_block(self.sym_time, self.sps, beta, adjust_beta=adjust_beta)
+        self.beta = self.sig_block.beta
+        self.photodiode_block = photodiode.Photodiode(self.responsivity, self.sigma2_sh, self.sigma2_th, 
+                                                      rng_seed, ideal=self.ideal)
+        self.int_dump_block = integrate_dump.Integrate_dump_block(self.sig_block)
+        self.detector_block = detector.Detector_block(self.sym_time, self.sig_block.Ts, self.beta, self.sym_block_len,
+                                                      self.responsivity, self.sigma2_sh, self.sigma2_th)
+        
+    def simulate_transmission(self, N_sym_blocks, rop_dBm, rng_seed=None):
+        constellation = const_mk.normalize_constellation_x_dBm(self.constellation,rop_dBm)
+        self.class_rep_block.set_up_const_and_rep_class(constellation)
+        self.detector_block.set_representative_class(self.class_rep_block.representative_class)
+        rng = np.random.default_rng(rng_seed)
+        len_rep_class = len(self.class_rep_block.representative_class)
+        k_tx = rng.choice(len_rep_class, N_sym_blocks)
+        symbols = self.class_rep_block.get_symbol_blocks(k_tx)
+        tx_signal = self.sig_block.generate_signal(symbols)
+        rx_signal = self.photodiode_block.square_law_detection(tx_signal)
+        y,z = self.int_dump_block.integrate_dump(rx_signal)
+        k_rx = self.detector_block.decode_logliklyhood(y,z,N_sym_blocks)
+        ser = get_SER(k_tx, k_rx)
+        ber = get_BER(k_tx, k_rx, len_rep_class)
+        mi = get_MI(k_tx, k_rx, len_rep_class, self.sym_block_len)
+        return ser, ber, mi
+    
+    def save_results(self, file_name, **kwargs):
+        save_data = {"sym_block_len": self.sym_block_len,
+                     "baud_rate": self.baud_rate,
+                     "sym_time": self.sym_time,
+                     "sps": self.sps,
+                     "fs": self.fs,
+                     "responsivity": self.responsivity,
+                     "sigma2_sh": self.sigma2_sh,
+                     "sigma2_th": self.sigma2_th,
+                     "constellation": self.constellation}
+        for key, value in kwargs.items():
+            save_data[key] = value
+
+        pickle.dump(save_data, open(file_name, "wb"))
--- skripts/main.py
+++ skripts/main.py
@@ -1,90 +1,54 @@
 import time
 import numpy as np
-import matplotlib.pyplot as plt 
 
+from tukey_signaling import Tukey_signaling
 import constellation_maker as const_mk
-import class_representative
-import signaling
 import photodiode
-import integrate_dump
-import detector
-from channel_metrics import get_BER, get_MI, get_SER
+
+
+####################### Definition of the system and simulation #######################
+tukey_sig_system = Tukey_signaling(file_name="communications_system/representative_classes/2-4SQAM_n3_all.npy",
+                                   sym_block_len=3,
+                                   baud_rate=10e9,
+                                   sps=21,
+                                   responsivity=10,                                             # R_APD = M_APD*R_D
+                                   N0_sh=photodiode.get_N0_sh(M_APD=20, F=12.78, R_apd=10),
+                                   N0_th=photodiode.get_N0_th(Tk=300, RL=15),
+                                   constellation=const_mk.nr_np_SQAM([1,1+np.sqrt(2)],4),
+                                   ideal=False)
+
 
 betas = np.array([0.1,0.5,0.7,0.9])
-start = time.time()
-for beta in betas:
-    ########################## Problem definition #####################################
-    file_name = "communications_system/representative_classes/2-4SQAM_n3_all.npy"
-    sym_block_len = 3
-    baud_rate = 10e9
-    sym_time = 1/baud_rate
-    sps = 21
-    fs = (sps-1)/sym_time
-    R_apd = 10 # R_APD = M_APD*R_D
-    sigma2_sh = photodiode.get_sigma2_sh(M_APD=20, F=12.78, R_apd=10, BW_2side=fs)
-    sigma2_th = photodiode.get_sigma2_th(Tk=300, RL=15, BW_2side=fs)
-    ideal = False
-    optical_power_range = np.arange(-33,-4)
+rop_range_dBm = np.arange(-13,-4)
+N_sym_blocks = 100
+rng_seed = 55
 
-    N_sym_blocks = 100000#200_000
-    rng_seed = 55
+##################################### Simulation ######################################
+ser = np.empty((len(betas),len(rop_range_dBm)))
+ber = np.empty((len(betas),len(rop_range_dBm)))
+mi = np.empty((len(betas),len(rop_range_dBm)))
 
-    ########################## system blocks creation #################################
-    class_rep_block = class_representative.Class_representative_block(file_name,sym_block_len)
-    sig_block = signaling.Signaling_block(sym_time, sps, beta, adjust_beta=True)
-    beta = sig_block.beta
-    photodiode_block = photodiode.Photodiode(R_apd, sigma2_sh, sigma2_th, rng_seed, ideal=ideal)
-    int_dump_block = integrate_dump.Integrate_dump_block(sig_block)
-    detector_block = detector.Detector_block(sym_time, sig_block.Ts, beta, sym_block_len, R_apd, sigma2_sh, sigma2_th)
-
-    ########################## Simulation #####################################
-
-    # ser = np.empty_like(optical_power_range, dtype=float)
-    # ber = np.empty_like(optical_power_range, dtype=float)
-    mi = np.empty_like(optical_power_range, dtype=float)
-    for i,op_pow in enumerate(optical_power_range):
-        constellation = const_mk.nr_np_SQAM([1,1+np.sqrt(2)],4)
-        constellation = const_mk.normalize_constellation_x_dBm(constellation,op_pow)
-        class_rep_block.set_up_const_and_rep_class(constellation)
-        detector_block.set_representative_class(class_rep_block.representative_class)
-        s = time.time()
-        rng = np.random.default_rng(rng_seed)
-        k_tx = rng.choice(len(class_rep_block.representative_class), N_sym_blocks)
-        symbols = class_rep_block.get_symbol_blocks(k_tx)
-        tx_signal = sig_block.generate_signal(symbols)
-        rx_signal = photodiode_block.square_law_detection(tx_signal)
-        y,z = int_dump_block.integrate_dump(rx_signal)
-        k_rx = detector_block.decode_logliklyhood(y,z,N_sym_blocks)
-        e = time.time()
-        # ser[i] = get_SER(k_tx, k_rx)
-        # ber[i] = get_BER(k_tx, k_rx, len(class_rep_block.representative_class))
-        mi[i] = get_MI(k_tx, k_rx, len(class_rep_block.representative_class), 3)
-        print(f"\tsimulation for ROP = {op_pow} dBm done successfully")
-        print(f"\tsimulation time: {e-s: .3f} seconds")
-    
-
+start_time = time.time()
+for i,beta in enumerate(betas):
+    print(f"starting simulation for beta = {beta}")
+    tukey_sig_system.setup_system(beta, rng_seed, adjust_beta=True)
+    for j,rop_dBm in enumerate(rop_range_dBm):
+        print(f"\tstarting simulation for ROP = {rop_dBm} dBm")
+        sub_start_time = time.time()
+        ser[i,j],ber[i,j],mi[i,j] = tukey_sig_system.simulate_transmission(N_sym_blocks, rop_dBm, rng_seed)
+        sub_end_time = time.time()
+        print(f"\tsimulation for ROP = {rop_dBm} dBm done successfully")
+        print(f"\tsimulation time: {sub_end_time-sub_start_time: .3f} seconds")
     print(f"simulation for beta = {beta} done successfully")
-    # with open(f'hole_system_sim_results/2-4SQAM_n4_M256_b{beta*100: .0f}_SER.npy', 'wb') as f:
-    #     np.save(f, ser)
+end_time = time.time()
 
-    # with open(f'hole_system_sim_results/2-4SQAM_n4_M256_b{beta*100: .0f}_BER.npy', 'wb') as f:
-    #     np.save(f, ber)
-
-    with open(f'whole_system_sim_results/2-4SQAM_n3_M72_b{beta*100: .0f}_MI.npy', 'wb') as f:
-        np.save(f, mi)
-    
-
-end = time.time()
-with open(f"whole_system_sim_results/2-4SQAM_n3_M72_resume.txt", 'w') as f:
-    f.write(f"number of symbol blocks simulated: {N_sym_blocks}\n")
-    f.write(f"simulation time: {end-start: .0f} seconds\n")
-    f.write(f"simulated betas: {np.array2string(betas)}\n")
-    f.write(f"simulated optical power range in dBm: {np.array2string(optical_power_range)}\n")
-    f.write(f"baud rate: {baud_rate: .5e} Hz\n")
-    f.write(f"sps: {sps}\n")
-    f.write(f"beta: {beta: .5f}\n")
-    f.write(f"sigma2_sh: {sigma2_sh: .5e}\n")
-    f.write(f"sigma2_th: {sigma2_th: .5e}\n")
-    f.write(f"check numbers for rng: {rng.choice(1000,4)}\n")
-    f.write(f"random generator seed: {rng_seed}")
-
+################################## Saving results ###################################
+tukey_sig_system.save_results("whole_system_sim_results/2-4SQAM_n3_all.p",
+                              N_sym_blocks=N_sym_blocks,
+                              betas=betas,
+                              rop_range_dBm=rop_range_dBm,
+                              ser=ser,
+                              ber=ber,
+                              mi=mi,
+                              sim_time=end_time-start_time,
+                              rng_seed=rng_seed)
